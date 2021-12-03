9ac848e2b4a7e4795daffcbfb26cbbe264275d44
anchikai <95461402+anchikai@users.noreply.github.com>
2021-12-03 00:21:07 -0500

    Create eid.lua

---
+++ lua/eid.lua
@@ -0,0 +1,43 @@
+--Esc Key
+EID:addCollectible(CollectibleType.COLLECTIBLE_ESC_KEY, "If player has fewer than 6 hearts, heals them with combination of red and soul hearts#Teleports you out of the room", "Esc Key")
+EID:addCollectible(CollectibleType.COLLECTIBLE_ESC_KEY, "Si el juegador tiene menos de 6 corazones, será curado con una mezcla de Corazones rojos y Corazones de Alma#Te teletransporta fuera de la habitación", "Tecla Esc", "spa")
+
+--Tilde Key
+EID:addCollectible(CollectibleType.COLLECTIBLE_TILDE_KEY, "Grants a random debug effect for the current room", "Tilde Key")
+EID:addCollectible(CollectibleType.COLLECTIBLE_TILDE_KEY, "Otorga un efecto aleatorio de la consola de desarrollo", "Tecla Tilde", "spa")
+
+--Alt Key
+EID:addCollectible(CollectibleType.COLLECTIBLE_ALT_KEY, "Normal floors will become one of its alt variants#Alt floors will become one of its normal variants", "Alt Key")
+EID:addCollectible(CollectibleType.COLLECTIBLE_ALT_KEY, "El piso normal se convierte en una de sus variantes(p.e. Sótano -> Sótano en llamas)#Los pisos alternativos se transforman en pisos normales (p.e. Sótano en llamas -> Sótano)", "Tecla Alt", "spa")
+
+--Spacebar Key
+EID:addCollectible(CollectibleType.COLLECTIBLE_SPACEBAR_KEY, "Teleports you to the I AM ERROR room upon use", "Spacebar Key")
+EID:addCollectible(CollectibleType.COLLECTIBLE_SPACEBAR_KEY, "Te teletransporta a una habitación de I AM ERROR tras usarla", "Tecla Espacio", "spa")
+
+--Backspace Key
+EID:addCollectible(CollectibleType.COLLECTIBLE_BACKSPACE_KEY, "Opens the door you last came from", "Backspace Key")
+EID:addCollectible(CollectibleType.COLLECTIBLE_BACKSPACE_KEY, "Abre la última puerta por la que entraste", "Tecla Retroceso", "spa")
+
+--Q Key
+EID:addCollectible(CollectibleType.COLLECTIBLE_Q_KEY, "Functions as a 3 in one Placebo, Blank Card, and Clear Rune#Will use your pocket active for free", "Q Key")
+EID:addCollectible(CollectibleType.COLLECTIBLE_Q_KEY, "Funciona como Placebo, Runa Pura y Carta Blanca 3 en 1#También usará tu activo de bolsillo gratuitamente", "Tecla Q", "spa")
+
+--E Key
+EID:addCollectible(CollectibleType.COLLECTIBLE_E_KEY, "Spawns a Gigabomb", "E Key")
+EID:addCollectible(CollectibleType.COLLECTIBLE_E_KEY, "Genera una Gigabomba", "Tecla E", "spa")
+
+--C Key
+EID:addCollectible(CollectibleType.COLLECTIBLE_C_KEY, "Teleports you to a large Library with 7 books", "C Key")
+EID:addCollectible(CollectibleType.COLLECTIBLE_C_KEY, "Te teletransporta a una Biblioteca grande con 7 libros", "Tecla C", "spa")
+
+--Caps Key
+EID:addCollectible(CollectibleType.COLLECTIBLE_CAPS_KEY, "Makes Isaac large for the current room#Allows Isaac to walk over obstacles to destroy them#↑ +7 Damage up#↑ +3 Range up", "Caps Key")
+EID:addCollectible(CollectibleType.COLLECTIBLE_CAPS_KEY, "Vuelve a Isaac más grande durante la habitación#Permite que Isaac destruya obstáculos al caminar sobre ellos#↑ +7 de daño#↑ +3 de alcance", "Tecla Bloq Mayús", "spa")
+
+--Enter Key
+EID:addCollectible(CollectibleType.COLLECTIBLE_ENTER_KEY, "Attempts to open the Boss Rush door#↑ Ignores in-game time", "Enter Key")
+EID:addCollectible(CollectibleType.COLLECTIBLE_ENTER_KEY, "Intenta abrir la entrada a la Boss Rush#↑ Ignora el contador del juego", "Tecla Enter", "spa")
+
+--Shift Key
+EID:addCollectible(CollectibleType.COLLECTIBLE_SHIFT_KEY, "If player is normal, they will become tainted#Tainted will become normal", "Shift Key")
+EID:addCollectible(CollectibleType.COLLECTIBLE_SHIFT_KEY, "Si el jugador está en su forma regular, cambiará a su forma corrompida#La forma corrompida pasará a ser la forma regular", "Tecla Shift", "spa")
